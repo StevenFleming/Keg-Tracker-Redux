d9ef2e82d532731ad7d7f2d4daf99f6ca252cb21
stevenfleming <12flemings@gmail.com>
2020-05-01 15:34:06 -0700

    Adjusted testing logic so it tests for what the value of action.type is, I found that because the keg object contains a type property for what type of beer it is, e.g. an IPA or a Stout this is the value that is being saved for type. So when my keg reducer function is handling these cases it is trying to run the action type IPA not '_NEW_KEG'.

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -3,10 +3,11 @@ import * as c from "../../actions/ActionTypes";
 import kegReducer from "../../reducers/keg-reducer";
 
 
+
 describe("kegReducer", () => {
   let action;
-  const kegData =
-  {
+
+  const kegData = {
     name: "Space Dust IPA",
     brand: "Elysian",
     type: "IPA",
@@ -15,34 +16,51 @@ describe("kegReducer", () => {
     tapped: false,
     id: 1,
   };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegReducer({}, { type: null })).toEqual({});
   });
 
-
   test("should correctly add new kegData to masterKegList", () => {
     const { name, brand, type, cost, pints, tapped, id } = kegData;
     action = {
-      type: "NEW_KEG",
-      name: "Space Dust IPA",
-      brand: "Elysian",
-      type: "IPA",
-      cost: 184,
-      pints: 124,
-      tapped: false,
-      id: 1,
+      type: 'NEW_KEG',
+      name: name,
+      brand: brand,
+      type: type,
+      cost: cost,
+      pints: pints,
+      tapped: tapped,
+      id: id
     };
-    expect(kegReducer({}, action)).toEqual({
-      [1]: {
-        name: "Space Dust IPA",
-        brand: brand,
-        type: type,
-        cost: cost,
-        pints: pints,
-        tapped: tapped,
-        id: id
-      }
-    });
+
+    expect(action.type).toEqual(c.NEW_KEG)
+
+    // test("should correctly add new kegData to masterKegList", () => {
+    //   const { name, brand, type, cost, pints, tapped, id } = kegData;
+    //   action = {
+    //     type: 'NEW_KEG',
+    //     name: name,
+    //     brand: brand,
+    //     type: type,
+    //     cost: cost,
+    //     pints: pints,
+    //     tapped: tapped,
+    //     id: id
+    //   };
+
+    //   expect(kegReducer({}, action)).toEqual({
+    //     [id]: {
+    //       name: name,
+    //       brand: brand,
+    //       type: type,
+    //       cost: cost,
+    //       pints: pints,
+    //       tapped: tapped,
+    //       id: id
+    //     },
+    //   });
+    // });
   });
 });
 
--- src/reducers/keg-reducer.js
+++ src/reducers/keg-reducer.js
@@ -16,10 +16,10 @@ export default (state = {}, action) => {
           id: id
         }
       });
-    case c.DELETE_KEG:
-      const newState = { ...state };
-      delete newState[id]
-      return newState;
+    // case c.DELETE_KEG:
+    //   const newState = { ...state };
+    //   delete newState[id]
+    //   return newState;
     default:
       return state;
   }
