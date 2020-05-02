3eddc23c07749ddac2f6d3e7ecfc67cd3545b8a7
stevenfleming <12flemings@gmail.com>
2020-05-02 14:40:41 -0700

    Creates action type and reducer for deselecting a selectedKeg

--- src/__tests__/actions/selectkeg.test.js
+++ src/__tests__/actions/selectkeg.test.js
@@ -2,47 +2,26 @@ import * as c from "../../actions/ActionTypes";
 import selectReducer from "../../reducers/select-reducer";
 
 describe("selectKegReducer", () => {
-
-
-  const currentState = {
-    1: {
-      name: "Space Dust IPA",
-      brand: "Elysian",
-      style: "IPA",
-      cost: 184,
-      pints: 124,
-      tapped: false,
-      id: 1,
-    },
-    2: {
-      name: "Racer 5",
-      brand: "Bear Republic",
-      style: "IPA",
-      cost: 165,
-      pints: 124,
-      tapped: false,
-      id: 2,
-    }
+  const keg =
+  {
+    name: "Racer 5",
+    brand: "Bear Republic",
+    style: "IPA",
+    cost: 165,
+    pints: 124,
+    tapped: false,
+    id: 2,
   }
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(selectReducer({}, { type: null })).toEqual({});
   });
 
-  test('Should successfully add selected keg to selecteKeg key value in state', () => {
-    let action = {
-      type: c.SELECT_KEG,
-      id: 2
-    };
-    expect(selectReducer(currentState, action)).toEqual({
-      selectedKeg: {
-        name: "Racer 5",
-        brand: "Bear Republic",
-        style: "IPA",
-        cost: 165,
-        pints: 124,
-        tapped: false,
-        id: 2,
-      }
-    });
+  test("Should return keg object", () => {
+    expect(selectReducer(keg, { type: c.SELECT_KEG })).toEqual(keg);
+  });
+
+  test("Should deselect keg and return a null", () => {
+    expect(selectReducer(keg, { type: c.DESELECT_KEG })).toEqual(null)
   });
 });
+
--- src/reducers/select-reducer.js
+++ src/reducers/select-reducer.js
@@ -1,52 +1,27 @@
 import * as c from "../actions/ActionTypes";
 
-export default (state = selectedKeg, action) => {
+export default (state = null, action) => {
   switch (action.type) {
     case c.SELECT_KEG:
-      const newerState = { ...state };
-
-      return newerState[action.id];
+      return state;
+    case c.DESELECT_KEG:
+      return null
     default:
       return state;
   }
 };
 
-const currentState = {
-  1: {
-    name: "Space Dust IPA",
-    brand: "Elysian",
-    style: "IPA",
-    cost: 184,
-    pints: 124,
-    tapped: false,
-    id: 1,
-  },
-  2: {
-    name: "Racer 5",
-    brand: "Bear Republic",
-    style: "IPA",
-    cost: 165,
-    pints: 124,
-    tapped: false,
-    id: 2,
-  }
-}
-selectedKeg: null,
 
-  test('Should successfully add selected keg to selecteKeg key value in state', () => {
-    action = {
-      type: c.SELECT_KEG,
-      id: 2
-    };
-    expect(kegReducer(currentState, action)).toEqual({
-      selectedKeg: {
-        name: "Racer 5",
-        brand: "Bear Republic",
-        style: "IPA",
-        cost: 165,
-        pints: 124,
-        tapped: false,
-        id: 2,
-      }
-    });
-  });
+
+// const { name, brand, style, cost, pints, tapped, id } = action;
+// return Object.assign({}, state, {
+//   [id]: {
+//     name: name,
+//     brand: brand,
+//     style: style,
+//     cost: cost,
+//     pints: pints,
+//     tapped: tapped,
+//     id: id
+//   }
+// });
