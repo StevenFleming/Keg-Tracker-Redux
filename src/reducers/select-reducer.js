54cf4bf117fd875e68fc57313b23b99a0201fcb7
stevenfleming <12flemings@gmail.com>
2020-05-02 11:10:10 -0700

    refractors testing logic for selectKeg

--- src/reducers/select-reducer.js
+++ src/reducers/select-reducer.js
@@ -1,31 +1,52 @@
 import * as c from "../actions/ActionTypes";
 
-const masterKegList = [
-  {
+export default (state = selectedKeg, action) => {
+  switch (action.type) {
+    case c.SELECT_KEG:
+      const newerState = { ...state };
+
+      return newerState[action.id];
+    default:
+      return state;
+  }
+};
+
+const currentState = {
+  1: {
     name: "Space Dust IPA",
     brand: "Elysian",
     style: "IPA",
     cost: 184,
     pints: 124,
     tapped: false,
-    id: "fakeId1",
+    id: 1,
   },
-
-  {
-    name: "Piny the Elder",
-    brand: "Russian River Brewing",
+  2: {
+    name: "Racer 5",
+    brand: "Bear Republic",
     style: "IPA",
-    cost: 204,
+    cost: 165,
     pints: 124,
     tapped: false,
-    id: "fakeId2",
-  },
-];
-
-export default (state = masterKegList, action) => {
-  switch (action.type) {
-    case c.SELECT_KEG:
-    default:
-      return state;
+    id: 2,
   }
-};
+}
+selectedKeg: null,
+
+  test('Should successfully add selected keg to selecteKeg key value in state', () => {
+    action = {
+      type: c.SELECT_KEG,
+      id: 2
+    };
+    expect(kegReducer(currentState, action)).toEqual({
+      selectedKeg: {
+        name: "Racer 5",
+        brand: "Bear Republic",
+        style: "IPA",
+        cost: 165,
+        pints: 124,
+        tapped: false,
+        id: 2,
+      }
+    });
+  });
